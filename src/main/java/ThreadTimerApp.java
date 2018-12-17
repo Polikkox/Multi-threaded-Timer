986cf363408b24c5dc9e5b019e064b50ff651aec
Polikkox <damian.targosz11@gmail.com>
2018-12-17 19:49:36 +0100

    Add create threads and interrupt them

--- src/main/java/ThreadTimerApp.java
+++ src/main/java/ThreadTimerApp.java
@@ -1,16 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ThreadTimerApp {
 
     private final List<Thread> threadList = new ArrayList<>();
     private final List<Timer> timerList = new ArrayList<>();
 
-    public void start(ConsoleCommandsInt consoleCommands){
-        boolean run = true;
+    public void start() {
+        boolean isRun = true;
+        Scanner scanner = new Scanner(System.in);
 
-        while (run){
-            String input = consoleCommands.enterCommand();
+        while (isRun){
+            Message.message("Enter command: ");
+            String command = scanner.nextLine();
+            String input = ParseInput.getCommand(command);
+
+            if(input.equals("start")){
+                runNewThread(command);
+            }
+            else if(input.equals("exit")){
+                exitThreads();
+            }
         }
+    }
+    private void runNewThread(String input){
+        String threadName = ParseInput.getValue(input);
+        Timer timer = new Timer(threadName);
+        Thread thread = new Thread(timer, threadName);
+        this.threadList.add(thread);
+        this.timerList.add(timer);
+        thread.start();
+    }
+
+
+    private void exitThreads() {
+        threadList.forEach(thread -> thread.interrupt());
+
+        threadList.forEach(s-> {
+            try {
+                s.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        timerList.forEach(System.out::println);
+        timerList.clear();
     }
 }
--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,7 +1,39 @@
 public class Timer implements Runnable{
+    private String name;
+    private int id;
+    private static int idThread = 0;
+    private long startTime;
+    private long elapsedTime;
+
+    public Timer(String name) {
+        this.name = name;
+        id = idThread++;
+    }
+
+
 
     @Override
     public void run() {
+        startTime = System.nanoTime();
+        while (true){
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {
+                stopTime();
+                throw new RuntimeException(e);
+            }
+        }
+
 
     }
+    public void stopTime(){
+        Long endTime = System.nanoTime();
+        elapsedTime = (endTime - startTime) / 1000000000;
+    }
+
+    public String toString(){
+        return "Thread : " + this.name + "\nThread id: " + this.id + "\nSeconds: " + this.elapsedTime;
+    }
+
 }
